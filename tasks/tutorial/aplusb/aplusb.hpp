cae1eb0df365f8551b3a583a703f97ec66c9abe9
DanT3r1 <daniilteresonok70@gmail.com>
2025-09-08 21:17:23 +0300

    Update aplusb.hpp

--- tasks/tutorial/aplusb/aplusb.hpp
+++ tasks/tutorial/aplusb/aplusb.hpp
@@ -1,5 +1,17 @@
+#include <cstdint>
 #include <iostream>
 
-int Sum(int /*a*/, int /*b*/) {
-    std::abort();  // Not implemented.
+int Sum(int a, int b) {
+    int64_t la = static_cast<int64_t>(a);
+    int64_t lb = static_cast<int64_t>(b);
+    int64_t result = la + lb;
+
+    if (result > INT_MAX) {
+        return INT_MAX;
+    }
+
+    if (result < INT_MIN) {
+        return INT_MIN;
+    }
+    return static_cast<int>(result);
 }
